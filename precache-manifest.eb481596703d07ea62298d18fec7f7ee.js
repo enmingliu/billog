3d48e80ceac9e393a48f8baf74a2c3b6752c393f
Bill Liu <bill.liu.school@gmail.com>
2019-03-28 22:53:26 -0700

    Updates

---
+++ precache-manifest.eb481596703d07ea62298d18fec7f7ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7996d097eb4db080fef1",
+    "url": "/billog/static/js/runtime~main.7996d097.js"
+  },
+  {
+    "revision": "ac42a50bcd6a821ccbae",
+    "url": "/billog/static/js/main.ac42a50b.chunk.js"
+  },
+  {
+    "revision": "e77c096c51c4ec2df626",
+    "url": "/billog/static/js/2.e77c096c.chunk.js"
+  },
+  {
+    "revision": "ac42a50bcd6a821ccbae",
+    "url": "/billog/static/css/main.e2c4e212.chunk.css"
+  },
+  {
+    "revision": "c17489e5f7eb4ca42ac8153ea3cbb8c9",
+    "url": "/billog/index.html"
+  }
+];
